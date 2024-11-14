c39581ce0fdf0dc18c00ff678cb300a24eb2d52c
RanaBeka <217ramram@gmail.com>
2024-11-14 21:02:47 +0100

    lol

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -12,7 +12,7 @@ public class Spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemigoPrefab, puntosSpawn, Quaternion.identity);
+        ///Instantiate(enemigoPrefab, puntosSpawn, Quaternion.identity);
     }
 
    
